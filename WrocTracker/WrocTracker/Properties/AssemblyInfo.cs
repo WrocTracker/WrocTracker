2978fe0779229fcd5b425f314b05dd798936544b
Piotrek Śródka <piotr.srodka@smtsoftware.com>
2015-09-03 17:24:32 +0200

    Synced with bitbucket

--- WrocTracker/WrocTracker/Properties/AssemblyInfo.cs
+++ WrocTracker/WrocTracker/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WrocTracker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WrocTracker")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7047d0b2-6823-4ece-936a-949a915d04a8")]
+[assembly: Guid("952f837d-8027-44a8-9e17-e0c3301a656c")]
 
 // Version information for an assembly consists of the following four values:
 //
